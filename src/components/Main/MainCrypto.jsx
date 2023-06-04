4d4edfcdf1d3d8b1f13c511fb191a3dc7f83596c
Zohaib <zohaibsoomro100@gmail.com>
2023-06-04 16:04:56 +0500

    Auto Reloading feature Added

--- src/components/Main/MainCrypto.jsx
+++ src/components/Main/MainCrypto.jsx
@@ -1,14 +1,79 @@
+// import React, { useEffect, useState } from "react";
+// import "./maincrypto.css";
+// import Coin from "../Coin/Coin";
+
+// const MainCrypto = () => {
+//   const [listOfCoins, setListOfCoins] = useState([]);
+//   const [searchWord, setSearchWord] = useState("");
+//   useEffect(() => {
+//     fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=pkr")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setListOfCoins(data);
+//         console.log(data);
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching data:", error);
+//       });
+//   }, []);
+
+//   const searchFilter = listOfCoins.filter((coin) => {
+//     return coin.name.toLowerCase().includes(searchWord.toLowerCase());
+//   });
+
+//   return (
+//     <div className="cryptoDisplay">
+//       <input
+//         type="text"
+//         placeholder="search"
+//         onChange={(event) => {
+//           setSearchWord(event.target.value);
+//         }}
+//       />
+//       <div className="display heading">
+//         <ul>
+//           <li style={{ width: "10rem" }}>NAME</li>
+//           <li>Symbol</li>
+//           <li>Current Price</li>
+//           <li>Icon</li>
+//         </ul>
+//       </div>
+//       {searchFilter.map((coin) => {
+//         return (
+//           <Coin
+//             name={coin.name}
+//             symbol={coin.symbol}
+//             current_price={coin.current_price}
+//             image={coin.image}
+//           />
+//         );
+//       })}
+//     </div>
+//   );
+// };
+
+// export default MainCrypto;
+
 import React, { useEffect, useState } from "react";
 import "./maincrypto.css";
 import Coin from "../Coin/Coin";
 
 const MainCrypto = () => {
   const [listOfCoins, setListOfCoins] = useState([]);
-  const [ searchWord, setSearchWord] = useState("")
+  const [searchWord, setSearchWord] = useState("");
+
   useEffect(() => {
-    fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=pkr"
-    )
+    fetchCoinData();
+
+    // Set the interval for reloading every 5 seconds
+    const interval = setInterval(fetchCoinData, 6000);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+
+  const fetchCoinData = () => {
+    fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=pkr")
       .then((response) => response.json())
       .then((data) => {
         setListOfCoins(data);
@@ -16,16 +81,23 @@ const MainCrypto = () => {
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
+        // <h2>{`Error fetching data:, ${error}`}</h2>
       });
-  }, []);
+  };
 
   const searchFilter = listOfCoins.filter((coin) => {
-    return coin.name.includes(searchWord);
-  })
+    return coin.name.toLowerCase().includes(searchWord.toLowerCase());
+  });
 
   return (
     <div className="cryptoDisplay">
-        <input type="text" placeholder="search" onChange={(event) => {setSearchWord(event.target.value)}}/>
+      <input
+        type="text"
+        placeholder="search"
+        onChange={(event) => {
+          setSearchWord(event.target.value);
+        }}
+      />
       <div className="display heading">
         <ul>
           <li style={{ width: "10rem" }}>NAME</li>
@@ -37,6 +109,7 @@ const MainCrypto = () => {
       {searchFilter.map((coin) => {
         return (
           <Coin
+            key={coin.id}
             name={coin.name}
             symbol={coin.symbol}
             current_price={coin.current_price}
